e1fffd3c43276bd1ecfb202dd27bc638b271b801
Cook Green <dell20091234@gmail.com>
2018-11-30 17:54:09 +0800

    Single Server Fetcher

--- service/singleserverfetcher.php
+++ service/singleserverfetcher.php
@@ -6,7 +6,10 @@ if(isset($_GET["ip"]))
 	$ip=$_GET["ip"];
 	$ip_array = array();
 	$ip_array[0] = $ip;
-	$si=ServerInfoFetcher::FetchAllServerDetailsReturnJson($ip_array);
+	
+	$fetcher = new ServerInfoFetcher();
+	
+	$si=$fetcher->FetchAllServerDetailsReturnJson($ip_array);
 	echo json_encode(json_decode($si)[0]);
 }
 ?>
